14d86381559e4342b13c995487e58964f8a9bc70
marcelozsilveira <marcelosilveira00@outlook.com>
2024-05-08 16:00:31 -0300

    update

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('ELISABETE.xml')
 root = tree.getroot()
 total = []
+completo = {}
 for item in root.iter('ITEM'):
     total.append(item.attrib['DESCRICAO'])
 for i in total:
-    print(i, total.count(i))
-    while i in total:
-        total.remove(i)
+    completo[i] = total.count(i)
+for k, v in completo.items():
+    print(k, ' -> ', v)
