45143c3292c1f69f6f62ac3c41c17670a53b82b2
marcelozsilveira <marcelosilveira00@outlook.com>
2024-05-09 11:35:54 -0300

    update

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import xml.etree.ElementTree as ET
+import os
 
-tree = ET.parse('ELISABETE.xml')
-root = tree.getroot()
-total = []
-completo = {}
-for item in root.iter('ITEM'):
-    total.append(item.attrib['DESCRICAO'])
-for i in total:
-    completo[i] = total.count(i)
-for k, v in completo.items():
-    print(k, ' -> ', v)
+
+def ler_xml(arquivo_xml):
+
+    with open(f'ferragens/{arquivo_xml}', 'rb') as ferragem:
+
+        tree = ET.parse(ferragem)
+        root = tree.getroot()
+        total = []
+        completo = {}
+
+        for item in root.iter('ITEM'):
+            total.append(item.attrib['DESCRICAO'])
+
+        for i in total:
+            completo[i] = total.count(i)
+
+        print(f'---> {arquivo_xml}\n')
+        for k, v in completo.items():
+            print(k, ' -> ', v)
+        print()
+        return completo
+
+
+lista_ferragens = os.listdir('ferragens')
+
+for ferragem in lista_ferragens:
+    ler_xml(ferragem)
